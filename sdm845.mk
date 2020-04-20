08c1fff865de8f19cd25339d8a60d0f1e4233823
shivatejapeddi <shivatejapeddi@gmail.com>
2020-04-20 09:11:20 +0530

    sdm845-common : Update Overlays for AEX

--- sdm845.mk
+++ sdm845.mk
@@ -16,12 +16,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
